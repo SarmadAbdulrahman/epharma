ca286555a4c0d4daa8990f1f7de6221ffb904cee
SarmadAbdulrahman <sarmood@outlook.com>
2021-01-17 11:56:11 +0300

    2021 adding drawaer

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -10,6 +10,13 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //print(_loginController.emailTextController.text);
+
+    int _selectedDestination = 0;
+
+
+
+
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -17,6 +24,62 @@ class HomeView extends StatelessWidget {
           'Dashboard',
         ),
       ),
+
+      drawer: Drawer(
+
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Header',
+               // style: textTheme.headline6,
+              ),
+            ),
+            Divider(
+              height: 1,
+              thickness: 1,
+            ),
+            ListTile(
+              leading: Icon(Icons.favorite),
+              title: Text('Item 1'),
+              selected: _selectedDestination == 0,
+            //  onTap: () => selectDestination(0),
+            ),
+            ListTile(
+              leading: Icon(Icons.delete),
+              title: Text('Item 2'),
+              selected: _selectedDestination == 1,
+            //  onTap: () => selectDestination(1),
+            ),
+            ListTile(
+              leading: Icon(Icons.label),
+              title: Text('Item 3'),
+              selected: _selectedDestination == 2,
+            //  onTap: () => selectDestination(2),
+            ),
+            Divider(
+              height: 1,
+              thickness: 1,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Label',
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.bookmark),
+              title: Text('Item A'),
+              selected: _selectedDestination == 3,
+            //  onTap: () => selectDestination(3),
+            ),
+          ],
+        ),
+      ),
+
       body: Container(
         padding: EdgeInsets.all(16),
         child: Column(
